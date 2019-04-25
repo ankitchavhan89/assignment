f5d40e5582e1b6775b59df7ec5715b28aa308124
Ankit chavhan <ankit.chavhan89@gmail.com>
2019-04-25 12:39:43 +0530

    Add files via upload

---
+++ src/config/constants.js
@@ -0,0 +1,16 @@
+module.exports = {
+    errorMessage: {
+        SUCCESS: 'SUCCESS',
+        FAILED: 'FAILED',
+        ACCESS_DENIED: 'ACCESS_DENIED',
+        USER_SUCCESSFULLY_REGISTERED: 'User registered suucessfully',
+        EMAIL_ALREADY_EXISTS: 'Email already exists',
+        EMAIL_NOT_ACTIVATED: 'Email not activated',
+        LOGGED_IN_SUCCESSFULLY: 'User logged in successfully',
+        NOTRADES: 'There are no trades in the given date range',
+        TRADESDELETED: 'All trades are deleted'
+    },
+    version: {
+        v1: 'v1.0.0',
+    },    
+};
---
+++ src/config/vars.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+/**
+ *  import .env variables
+ */
+
+require('dotenv-safe').load({
+    allowEmptyValues: true,
+    path: path.join(__dirname, '../../.env'),
+    sample: path.join(__dirname, '../../.env.example'),
+});
+
+module.exports = {
+    env: process.env.NODE_ENV,
+    port: process.env.PORT,
+    apiLink: `http://${process.env.HOST}:${process.env.PORT}`,
+    basePath: '/api/v1',
+    jwtSecret: process.env.JWT_SECRET,
+    jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES,
+    mongo: {
+        uri: process.env.NODE_ENV === 'test'
+            ? process.env.MONGO_URI_TESTS : process.env.MONGO_URI,
+        enabled: process.env.MONGO_ENABLED,
+    },
+    logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
+    userIDLength: process.env.USER_ID_LENGTH,
+};
